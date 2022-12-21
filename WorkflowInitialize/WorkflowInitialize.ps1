fc4a8f96d347b840affafc6239819ac8e616e7d3
Freddy Dan Dalgas Kristiansen <74459681+FreundDK@users.noreply.github.com>
2022-12-21 05:18:35 +0100

    Update WorkflowInitialize.ps1

--- WorkflowInitialize/WorkflowInitialize.ps1
+++ WorkflowInitialize/WorkflowInitialize.ps1
@@ -10,6 +10,8 @@ $BcContainerHelperPath = ""
 
 # THIS IS MYCHANGES BRANCH
 
+# HELLO WORLD
+
 # IMPORTANT: No code that can fail should be outside the try/catch
 try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
