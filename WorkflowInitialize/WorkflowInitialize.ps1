adca8ddb8625b001128acbd03d7de0af2c71bfa8
FreundDK <FreundDK@users.noreply.github.com>
2022-12-21 04:14:34 +0000

    checkout

--- WorkflowInitialize/WorkflowInitialize.ps1
+++ WorkflowInitialize/WorkflowInitialize.ps1
@@ -8,6 +8,8 @@ Set-StrictMode -Version 2.0
 $telemetryScope = $null
 $BcContainerHelperPath = ""
 
+# THIS IS MYCHANGES BRANCH
+
 # IMPORTANT: No code that can fail should be outside the try/catch
 try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
